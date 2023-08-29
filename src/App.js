c62cf4f23fd638af855214e9910b46a423e6724f
Nalu Media <ryan.dranginis@gmail.com>
2023-08-29 16:28:59 -0700

    added ga4 and set env variables

--- src/App.js
+++ src/App.js
@@ -2,18 +2,20 @@ import React, { useState, useEffect } from 'react';
 import client from './Contentful';
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 import { BLOCKS, MARKS } from '@contentful/rich-text-types';
-import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link, Routes, useLocation } from 'react-router-dom';
 import PostDetails from './PostDetails';
 import CheckoutForm from './CheckoutForm'; 
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
 import About from './About';  // Ensure the path is correct
-import { Helmet } from 'react-helmet';  // Import react-helmet
+import { Helmet } from 'react-helmet';
+import ReactGA4 from 'react-ga4';  // Your GA import
 
 const stripePromise = loadStripe("YOUR_PUBLIC_KEY");
 
-function App() {
+function MainContent() {
   const [posts, setPosts] = useState([]);
+  const location = useLocation();
 
   useEffect(() => {
     client.getEntries({ content_type: 'blog' })
@@ -22,6 +24,11 @@ function App() {
       })
       .catch(console.error);
   }, []);
+
+  useEffect(() => {
+    ReactGA4.initialize('G-80L7YYXV7R');
+    ReactGA4.send('pageview');
+  }, [location]);
 
   const formatDate = (dateString) => {
     const options = { year: 'numeric', month: 'long', day: 'numeric' };
@@ -42,71 +49,70 @@ function App() {
   };
 
   return (
+    <div className="container">
+      <Helmet>
+        <title>All things Basketball Analytics ~ Hoopsbot</title>
+        <meta name="description" content="Dive deep into the world of basketball analytics with Hoopsbot. Explore in-depth statistics, game insights, player performance metrics, and the latest trends shaping the court. Elevate your understanding of the game beyond the scoreboard." />
+      </Helmet>
+
+      <header className="d-flex justify-content-between align-items-center my-4">
+        <Link to="/" className="text-decoration-none">
+          <h1 className="logo">Hoopsbot 🏀🤖</h1>
+        </Link>
+        <input type="text" placeholder="Search..." className="form-control w-50" />
+      </header>
+
+      <nav className="mb-5">
+        <ul className="nav">
+          <li className="nav-item">
+            <Link to="/" className="nav-link">Blog</Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/about" className="nav-link">About</Link>
+          </li>
+          <li className="nav-item">
+            <Link to="#" className="nav-link">Contact</Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/checkout" className="nav-link">Checkout</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Elements stripe={stripePromise}>
+        <Routes>
+          <Route path="/post/:id" element={<PostDetails />} />
+          <Route path="/checkout" element={<CheckoutForm />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/" element={
+            <>
+              <main>
+                {posts.map(post => (
+                  <div key={post.sys.id} className="mb-5">
+                    <Link to={`/post/${post.sys.id}`} className="text-decoration-none text-dark">
+                      <h2>{post.fields.title}</h2>
+                    </Link>
+                    {documentToReactComponents(post.fields.body, options)}
+                    <p><strong>Published on:</strong> {formatDate(post.fields.published)}</p>
+                  </div>
+                ))}
+              </main>
+            </>
+          } />
+        </Routes>
+      </Elements>
+
+      <footer className="mt-5">
+        <p>© 2023 Hoopsbot. All rights reserved.</p>
+      </footer>
+    </div>
+  );
+}
+
+function App() {
+  return (
     <Router>
-      <div className="container">
-
-        {/* React Helmet for Meta Tags */}
-        <Helmet>
-          <title>All things Basketball Analytics ~ Hoopsbot</title>
-          <meta name="description" content="Dive deep into the world of basketball analytics with Hoopsbot. Explore in-depth statistics, game insights, player performance metrics, and the latest trends shaping the court. Elevate your understanding of the game beyond the scoreboard." />
-        </Helmet>
-
-        {/* Header */}
-        <header className="d-flex justify-content-between align-items-center my-4">
-          <Link to="/" className="text-decoration-none">
-            <h1 className="logo">Hoopsbot 🏀🤖</h1>
-          </Link>
-          <input type="text" placeholder="Search..." className="form-control w-50" />
-        </header>
-
-        {/* Navigation */}
-        <nav className="mb-5">
-          <ul className="nav">
-            <li className="nav-item">
-              <Link to="/" className="nav-link">Blog</Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/about" className="nav-link">About</Link>
-            </li>
-            <li className="nav-item">
-              <Link to="#" className="nav-link">Contact</Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/checkout" className="nav-link">Checkout</Link>
-            </li>
-          </ul>
-        </nav>
-
-        {/* Main Content */}
-        <Elements stripe={stripePromise}>
-          <Routes>
-            <Route path="/post/:id" element={<PostDetails />} />
-            <Route path="/checkout" element={<CheckoutForm />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/" element={
-              <>
-                {/* Blog Posts */}
-                <main>
-                  {posts.map(post => (
-                    <div key={post.sys.id} className="mb-5">
-                      <Link to={`/post/${post.sys.id}`} className="text-decoration-none text-dark">
-                        <h2>{post.fields.title}</h2>
-                      </Link>
-                      {documentToReactComponents(post.fields.body, options)}
-                      <p><strong>Published on:</strong> {formatDate(post.fields.published)}</p>
-                    </div>
-                  ))}
-                </main>
-              </>
-            } />
-          </Routes>
-        </Elements>
-
-        {/* Footer */}
-        <footer className="mt-5">
-          <p>© 2023 Hoopsbot. All rights reserved.</p>
-        </footer>
-      </div>
+      <MainContent />
     </Router>
   );
 }
--- src/Contentful.js
+++ src/Contentful.js
@@ -1,9 +1,18 @@
 // Contentful.js
+// import { createClient } from 'contentful';
+
+// const client = createClient({
+//   space: 'qzwnil1y1qss', // Replace with your Space ID
+//   accessToken: 'Fpiwp5bs5R4-7kThDg0eJZemA_gDfBVkUHADoF1qlxg' // Replace with your Access Token
+// });
+
+// export default client;
+
 import { createClient } from 'contentful';
 
 const client = createClient({
-  space: 'qzwnil1y1qss', // Replace with your Space ID
-  accessToken: 'Fpiwp5bs5R4-7kThDg0eJZemA_gDfBVkUHADoF1qlxg' // Replace with your Access Token
+  space: process.env.REACT_APP_CONTENTFUL_SPACE,
+  accessToken: process.env.REACT_APP_CONTENTFUL_ACCESS_TOKEN
 });
 
 export default client;
