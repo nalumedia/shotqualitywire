7d3c4b14cada8db27fbb159407d46825f92f6016
Nalu Media <ryan.dranginis@gmail.com>
2023-08-29 16:37:41 -0700

    added logger part deaux

--- src/Contentful.js
+++ src/Contentful.js
@@ -11,11 +11,7 @@
 import { createClient } from 'contentful';
 
 console.log("Access Token:", process.env.REACT_APP_CONTENTFUL_ACCESS_TOKEN);
-const client = createClient({
-  space: process.env.REACT_APP_CONTENTFUL_SPACE,
-  accessToken: process.env.REACT_APP_CONTENTFUL_ACCESS_TOKEN
-});
-
+console.log("Space:", process.env.REACT_APP_CONTENTFUL_SPACE);
 
 const client = createClient({
   space: process.env.REACT_APP_CONTENTFUL_SPACE,
